797761009f65722beaef83124022ad7efb0bc79b
Zarrouk18 <zarrouk.medaziz@esprit.tn>
2022-03-09 23:41:33 +0100

    MAJ foch

---
+++ fonction.h
@@ -0,0 +1,7 @@
+
+void intEnnemi(Ennemi*e);
+void afficherEnnemi(Ennemi e,SDL_Surface * screen);
+void animerEnnemi( Ennemi * e);
+void deplacer(Ennemi * e);
+int collisionBB(personne p, Ennemi e);
+void deplacerIA(Ennemi *e,SDL_Rect posPerso);
